f90997555ca4e307624d847490494b94c08fe082
DeyanTopalov <deyan.topalow@gmail.com>
2023-05-07 14:07:00 +0300

    search bar functionality

--- src/App.js
+++ src/App.js
@@ -4,17 +4,64 @@ import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import ParticlesBg from 'particles-bg'
+import { Component } from 'react'; //Convert to 
+// import { useState } from 'react'; -> when using the Function
 
-function App() {
+
+// function App() { // Using Function instead of Class Component
+//   const [input, setInput] = useState('');
+
+
+// const onInputChange = (event) => {
+//   console.log(event.target.value);
+// }
+
+// const onButtonSubmit = () => {
+//   console.log('click');
+// }
+
+//   return (
+//     <div className="App">
+//       <ParticlesBg type="cobweb" num='500' bg={true} />
+//       <Navigation />
+//       <Logo />
+//       <Rank />
+//       <ImageLinkForm onInputChange = {onInputChange} 
+//       onButtonSubmit = {onButtonSubmit}/>
+//     </div>
+//   );
+// }
+
+
+class App extends Component { // using Class Component
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+    }
+  }
+
+onInputChange = (event) => {
+  console.log(event.target.value);
+}
+
+onButtonSubmit = () => {
+  console.log('click');
+}
+
+render() {
   return (
     <div className="App">
       <ParticlesBg type="cobweb" num='500' bg={true} />
       <Navigation />
       <Logo />
       <Rank />
-      <ImageLinkForm />
+      <ImageLinkForm onInputChange = {this.onInputChange} 
+      onButtonSubmit = {this.onButtonSubmit}/>
     </div>
   );
 }
+}
+ 
 
 export default App;
--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './ImageLinkForm.css';
 
 
-const ImageLinkForm = () => {
+const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
     return (
         <div>
           <p className ='f2'>
@@ -11,8 +11,8 @@ const ImageLinkForm = () => {
           </p>
           <div className='center'>
             <div className='form center pa4 br3 shadow-5'>
-            <input className ='f4 pa2 w-60 center' type='text' />
-            <button className='center w-25 grow f3 link ph3 pv1 dib white bg-blue'>
+            <input className ='f4 pa2 w-60 center' type='text' onChange={onInputChange} />
+            <button className='center w-25 grow f3 link ph3 pv1 dib white bg-blue' onClick={onButtonSubmit}>
                 Detect
             </button>
             </div>
