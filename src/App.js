0d6b4c6ac871b014aea0bd57a827447cb42783e1
DeyanTopalov <deyan.topalow@gmail.com>
2023-05-13 14:50:59 +0300

    Formatting fixes

--- src/App.js
+++ src/App.js
@@ -1,68 +1,73 @@
-import './App.css';
-import Navigation from './components/Navigation/Navigation';
-import Logo from './components/Logo/Logo';
-import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
-import Rank from './components/Rank/Rank';
-import FaceRecognition from './components/FaceRecognition/FaceRecognition';
-import ParticlesBg from 'particles-bg'
-import Signin from './components/Signin/Signin';
-import Register from './components/Register/Register';
-import { Component } from 'react'; 
+import "./App.css";
+import Navigation from "./components/Navigation/Navigation";
+import Logo from "./components/Logo/Logo";
+import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
+import Rank from "./components/Rank/Rank";
+import FaceRecognition from "./components/FaceRecognition/FaceRecognition";
+import ParticlesBg from "particles-bg";
+import Signin from "./components/Signin/Signin";
+import Register from "./components/Register/Register";
+import { Component } from "react";
 
-class App extends Component { // using Class Component
+class App extends Component {
+  // using Class Component
   constructor() {
     super();
     this.state = {
-      input: '',
-      imageUrl: '',
+      input: "",
+      imageUrl: "",
       box: {},
-      route: 'signin', // route state used to navigate 
-      isSignedIn: false //new state, add the false as boolean not a string
-    }
+      route: "signin", // route state used to navigate
+      isSignedIn: false, //new state, add the false as boolean not a string
+    };
   }
-
 
   onInputChange = (event) => {
-    this.setState({input: event.target.value});
-  }
+    this.setState({ input: event.target.value });
+  };
 
   onButtonSubmit = () => {
-    console.log('click');
-}
+    console.log("click");
+  };
 
-onRouteChange = (route) => { // change here with cond statements
-  if (route === 'signout') {
-    this.setState({isSignedIn: false})
-  } else if (route === 'home') {
-    this.setState({isSignedIn: true})
+  onRouteChange = (route) => {
+    // change here with cond statements
+    if (route === "signout") {
+      this.setState({ isSignedIn: false });
+    } else if (route === "home") {
+      this.setState({ isSignedIn: true });
+    }
+    this.setState({ route: route });
+  };
+
+  render() {
+    const { isSignedIn, imageUrl, route } = this.state;
+    return (
+      <div className="App">
+        <ParticlesBg type="cobweb" num="500" bg={true} />
+        <Navigation
+          isSignedIn={isSignedIn}
+          onRouteChange={this.onRouteChange}
+        />{" "}
+        {/* add the isSignedIn here */}
+        {route === "home" ? (
+          <div>
+            <Logo />
+            <Rank />
+            <ImageLinkForm
+              onInputChange={this.onInputChange}
+              onButtonSubmit={this.onButtonSubmit}
+            />
+            <FaceRecognition imageUrl={imageUrl} />
+          </div>
+        ) : route === "signin" ? (
+          <Signin onRouteChange={this.onRouteChange} />
+        ) : (
+          <Register onRouteChange={this.onRouteChange} />
+        )}
+      </div>
+    );
   }
-  this.setState({route: route})
 }
-
-render() {
-  const { isSignedIn, imageUrl, route } = this.state;
-  return (
-    <div className="App">
-      <ParticlesBg type="cobweb" num='500' bg={true} />
-      <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange}/> {/* add the isSignedIn here */}
-      { route === 'home' 
-        ? <div> 
-          <Logo />
-          <Rank />
-          <ImageLinkForm onInputChange = {this.onInputChange} 
-          onButtonSubmit = {this.onButtonSubmit}/>
-          <FaceRecognition imageUrl ={imageUrl} />
-        </div>
-        : (
-          route === 'signin'
-          ? <Signin onRouteChange={this.onRouteChange}/> 
-          : <Register onRouteChange={this.onRouteChange}/> 
-        )
-      }
-    </div>
-  );
-}
-}
- 
 
 export default App;
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,39 +1,60 @@
-import React from 'react';
+import React from "react";
 
 const Register = ({ onRouteChange }) => {
-    return (
-        <article className="br3 ba white white-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center"> {/* Card template */}
-            <main className="pa4 white">  
-                <div className="measure">
-                    <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
-                    <legend className="f1 fw6 ph0 mh0 white">Register</legend>
-                    <div className="mt3">
-                        <label className="db fw6 lh-copy f4" htmlFor="Name">Name</label>
-                        <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="Text" name="Name"  id="Name"/>
-                    </div>
-                    <div className="mt3">
-                        <label className="db fw6 lh-copy f4" htmlFor="email-address">Email</label>
-                        <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address"  id="email-address"/>
-                    </div>
-                    <div className="mv3">
-                        <label className="db fw6 lh-copy f4" htmlFor="password">Password</label>
-                        <input className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="password" name="password"  id="password"/>
-                    </div>
-                    </fieldset>
-                    <div className="">
-                    <input 
-                        onClick={() => onRouteChange('home')}
-                        className="b ph3 pv2 input-reset ba white bg-transparent grow pointer f4 dib" 
-                        type="submit" 
-                        value="Register"
-                    />
-                    </div>
-                </div>
-            </main>
-        </article>
-    
-
-    );
-}
+  return (
+    <article className="br3 ba white white-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
+      {" "}
+      {/* Card template */}
+      <main className="pa4 white">
+        <div className="measure">
+          <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
+            <legend className="f1 fw6 ph0 mh0 white">Register</legend>
+            <div className="mt3">
+              <label className="db fw6 lh-copy f4" htmlFor="Name">
+                Name
+              </label>
+              <input
+                className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                type="Text"
+                name="Name"
+                id="Name"
+              />
+            </div>
+            <div className="mt3">
+              <label className="db fw6 lh-copy f4" htmlFor="email-address">
+                Email
+              </label>
+              <input
+                className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                type="email"
+                name="email-address"
+                id="email-address"
+              />
+            </div>
+            <div className="mv3">
+              <label className="db fw6 lh-copy f4" htmlFor="password">
+                Password
+              </label>
+              <input
+                className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                type="password"
+                name="password"
+                id="password"
+              />
+            </div>
+          </fieldset>
+          <div className="">
+            <input
+              onClick={() => onRouteChange("home")}
+              className="b ph3 pv2 input-reset ba white bg-transparent grow pointer f4 dib"
+              type="submit"
+              value="Register"
+            />
+          </div>
+        </div>
+      </main>
+    </article>
+  );
+};
 
 export default Register;
