dd6c78e91113902bd3e95b5f9e93219048afd555
DeyanTopalov <deyan.topalow@gmail.com>
2023-05-28 18:16:47 +0300

    fixes

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -34,7 +34,7 @@ class Register extends React.Component {
     })
       .then((res) => res.json())
       .then((user) => {
-        if (user) {
+        if (user.id) {
           this.props.loadUser(user); // we need to create this in App.js
           this.props.onRouteChange("home");
         }
